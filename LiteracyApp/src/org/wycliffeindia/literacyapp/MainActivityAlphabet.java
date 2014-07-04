4b2ca767b313ecc143246770ec068bd55df207f8
Bibin <bibin_cherian@wycliffeindia.org>
2014-07-04 15:28:44 +0530

    test

--- LiteracyApp/src/org/wycliffeindia/literacyapp/MainActivityAlphabet.java
+++ LiteracyApp/src/org/wycliffeindia/literacyapp/MainActivityAlphabet.java
@@ -15,7 +15,7 @@ public class MainActivityAlphabet extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_activity_alphabet);
-		
+		//demo confict
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
