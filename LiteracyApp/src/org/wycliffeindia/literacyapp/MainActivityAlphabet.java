e2b920b89dd88ba18fd3ac0ff7f44287f31a16aa
Bibin <bibin_cherian@wycliffeindia.org>
2014-07-04 12:49:38 +0530

    Back button enabled - test

--- LiteracyApp/src/org/wycliffeindia/literacyapp/MainActivityAlphabet.java
+++ LiteracyApp/src/org/wycliffeindia/literacyapp/MainActivityAlphabet.java
@@ -1,7 +1,6 @@
 package org.wycliffeindia.literacyapp;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivityAlphabet extends Activity {
 
@@ -17,7 +15,9 @@ public class MainActivityAlphabet extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_activity_alphabet);
-
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
